e67085a7098ae026684968d38bb5ebf7ada25d89
zw <1165399238@qq.com>
2026-01-09 12:36:55 +0800

    htl-2000xt

---
+++ ruoyi-ui/src/settings.js
@@ -0,0 +1,56 @@
+module.exports = {
+  /**
+   * 网页标题
+   */
+  title: process.env.VUE_APP_TITLE,
+
+  /**
+   * 侧边栏主题 深色主题theme-dark，浅色主题theme-light
+   */
+  sideTheme: 'theme-dark',
+
+  /**
+   * 系统布局配置
+   */
+  showSettings: true,
+
+  /**
+   * 菜单导航模式 1、纯左侧 2、混合（左侧+顶部） 3、纯顶部
+   */
+  navType: 1,
+
+  /**
+   * 是否显示 tagsView
+   */
+  tagsView: true,
+
+  /**
+   * 显示页签图标
+   */
+  tagsIcon: false,
+
+  /**
+   * 是否固定头部
+   */
+  fixedHeader: true,
+
+  /**
+   * 是否显示logo
+   */
+  sidebarLogo: true,
+
+  /**
+   * 是否显示动态标题
+   */
+  dynamicTitle: false,
+
+  /**
+   * 是否显示底部版权
+   */
+  footerVisible: false,
+
+  /**
+   * 底部版权文本内容
+   */
+  footerContent: 'Copyright © 2022-2026 西安高能翠幕科技发展有限责任公司  地址：陕西省西安市高新技术产业开发区 电话：029-12345678 | 邮箱：info@ghcm.com.'
+}
