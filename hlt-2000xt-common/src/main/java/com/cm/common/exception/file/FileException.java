4a41b8c6cce4324376597c156e9e29c558fd86f7
zw <1165399238@qq.com>
2026-01-09 16:58:24 +0800

    update modelName

---
+++ hlt-2000xt-common/src/main/java/com/cm/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package com.cm.common.exception.file;
+
+import com.cm.common.exception.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
